7203b6badbb3036eb81f1b898dbc2d65872aa85a
trynxID <sidiq.firman48@gmail.com>
2024-05-28 09:42:19 +0700

    add error handling

--- app.js
+++ app.js
@@ -1,20 +1,40 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+// Package
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const logger = require("morgan");
+const bodyParser = require("body-parser");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// Router
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
 
-var app = express();
+// Define Express
+const app = express();
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// Middleware
+app.use(logger("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+
+// Handling Invalid Route
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// Error Handling
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  const errorResponse = {
+    message: err.message,
+    status: err.status,
+    stack: err.stack,
+  };
+  req.app.get("env") === "development" ? res.json(errorResponse) : {};
+});
 
 module.exports = app;
