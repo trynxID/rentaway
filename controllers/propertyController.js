5be24956759f3d9cee13ee1d1980abcbf3fa367a
DedeWahyu <dedeanugrah12@gmail.com>
2024-06-01 10:30:47 +0700

    add properties controller

--- controllers/propertyController.js
+++ controllers/propertyController.js
@@ -1,9 +1,76 @@
 const Property = require("../models/properties");
+const User = require("../models/users");
+const { validationResult } = require("express-validator");
+
 const getAllProperties = async (req, res) => {
   const properties = await Property.find();
   res.status(200);
   res.json(properties);
 };
+
+const addProperty = async (req, res) => {
+  // Validasi input
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  // Destructure data properti dari req.body
+  const {
+    title,
+    description,
+    price,
+    location,
+    category,
+    details,
+    capacity,
+    availability,
+    status
+  } = req.body;
+
+  try {
+    // Pastikan data tidak kosong
+    if (!title || !description || !price || !location || !category || !details || !capacity || !availability || !status) {
+      return res.status(400).json({ msg: "Semua data harus diisi" });
+    }
+
+      // Pastikan userId tersedia di req.body
+      if (!req.body.userId) {
+        return res.status(400).json({ msg: "ID pengguna (userId) diperlukan" });
+      }
+  
+      // Temukan pengguna dengan ID yang diberikan
+      const user = await User.findById(req.body.userId);
+  
+      // Pastikan pengguna dengan ID yang diberikan memiliki peran sebagai pemilik (role 2)
+      if (!user || user.role !== 2) {
+        return res.status(403).json({ msg: "Anda tidak memiliki izin untuk menambahkan properti" });
+      }
+
+    // Buat properti baru
+    const newProperty = new Property({
+      owner: req.body.userId, // Gunakan userId yang disertakan dalam req.body
+      title,
+      description,
+      price,
+      location,
+      category,
+      details,
+      capacity,
+      availability,
+      status,
+    });
+
+    // Simpan properti baru ke database
+    await newProperty.save();
+
+    res.status(201).json({ msg: "Properti berhasil ditambahkan" });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Kesalahan server");
+  }
+};
+
 module.exports = {
-  getAllProperties,
+  getAllProperties, addProperty
 };
--- routes/properties.js
+++ routes/properties.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const user = require("../controllers").property;
 
 router.get("/", user.getAllProperties);
+router.post("/add", user.addProperty);
 
 module.exports = router;
