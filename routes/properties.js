130c898be66fa69df6265f453fb0f2507d90b6d3
DedeWahyu <dedeanugrah12@gmail.com>
2024-06-07 14:41:21 +0700

    multer property

--- routes/properties.js
+++ routes/properties.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const { verifyToken } = require("../utils/token");
-const upload = require("../utils/multer");
+const {uploadProperty}  = require("../utils/multer");
 const property = require("../controllers").property;
 
 router.get("/", property.getAllProperties);
 router.post(
   "/add",
   verifyToken,
-  upload.array("images", 5),
+  uploadProperty.array("images", 5),
   property.addProperty
 );
 router.get("/detail/:id", verifyToken, property.getPropertyById);
